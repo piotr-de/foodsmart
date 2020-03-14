5448a69a4ee25a88c14fbffa4d0d879abb968039
Piotr Damer <piotrd@czuby.net>
2020-03-14 15:03:45 +0000

    Basic Python API call logic

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-    @recipes
+    @recipes = JSON.parse(File.read("python/recipes/pizza_100_to_199.json"))
+    # render json: @recipes
   end
 end
--- python/fetch_recipes.py
+++ python/fetch_recipes.py
@@ -1,5 +1,27 @@
 import requests
+import json
 
-data = requests.get("https://www.edamam.com/search?q=pizza&health=vegan&from=0&to=100").json()
+query = "pizza"
 
-return data
+n = 0
+m = 99
+
+url = f"https://www.edamam.com/search?q=pizza&health=vegan&from={n}&to={m}"
+
+data = requests.get(url)
+
+results = data.json()
+count = results["count"]
+
+with open(f'recipes/{query}_{n}_to_{m}.json', "w", encoding="utf-8") as f:
+  json.dump(results, f, ensure_ascii=False, indent=4)
+
+if count > 99:
+  n = n + 100
+  m = m + 100
+  while n <= count:
+    extra_data = requests.get(f"https://www.edamam.com/search?q=pizza&health=vegan&from={n}&to={m}").json()
+    with open(f'recipes/{query}_{n}_to_{m}.json', "w", encoding="utf-8") as f:
+      json.dump(extra_data, f, ensure_ascii=False, indent=4)
+    n = n + 100
+    m = m + 100
