d862af139095ee25e495ddcaba2ec981f451391a
Piotr Damer <piotrd@czuby.net>
2020-03-15 11:46:53 +0000

    DB seed parsing .json recipe files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+path = File.join(Rails.root, "static/recipes")
+files = []
+
+puts "Opening #{path}..."
+
+i = 1
+
+Dir.foreach(path) do |file|
+  next unless file.include?(".json")
+  files << file
+  print "#{i}..."
+  i += 1
+end
+
+puts "\nFiles read"
+
+files.sort!
+
+puts "Files sorted"
+
+files.each_with_index do |file, index|
+  puts "#{index + 1}. Parsing #{file}..."
+  json = File.read(File.join(path, file))
+  hits = JSON.parse(json)["hits"]
+  hits.each do |hit|
+    recipe_created = Recipe.create!(name: hit["recipe"]["label"], url: hit["recipe"]["url"], data: hit)
+    puts "Recipe \"#{recipe_created.name}\" created"
+  end
+end
