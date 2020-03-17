456a7b7d44ec4258ecf3121cd8d61d78bd6e1f0b
Piotr Damer <piotrd@czuby.net>
2020-03-17 20:47:15 +0000

    Ingredient: routes, controller and index view

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,6 +2,9 @@ class IngredientsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-    @ingredients
+    url = "https://api.edamam.com/api/food-database/parser?app_id=#{ENV['EDAMAM_APP_ID']}&app_key=#{ENV['EDAMAM_API_KEY']}&ingr=#{params[:query]}"
+    json = RestClient.get(url)
+    results = JSON.parse(json)
+    @ingredients = results
   end
 end
