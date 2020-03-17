19ddeebdae21bb76e987488bde5c3e3dc66f1f43
Piotr Damer <piotrd@czuby.net>
2020-03-17 22:38:28 +0000

    Ingredient filtering in the controller and pie chart display logic in the index view

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,9 +2,20 @@ class IngredientsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-    url = "https://api.edamam.com/api/food-database/parser?app_id=#{ENV['EDAMAM_APP_ID']}&app_key=#{ENV['EDAMAM_API_KEY']}&ingr=#{params[:query]}"
+    url = "https://api.edamam.com/api/food-database/parser?app_id=#{ENV['EDAMAM_APP_ID']}&app_key=#{ENV['EDAMAM_API_KEY']}&ingr=#{params[:query]}&health=vegan"
     json = RestClient.get(url)
     results = JSON.parse(json)
-    @ingredients = results
+    @ingredients = []
+    results["hints"].each do |result|
+      ingredient = Ingredient.new({
+        name: result["food"]["label"],
+        energy: result["food"]["nutrients"]["ENERC_KCAL"],
+        carbs: result["food"]["nutrients"]["CHOCDF"],
+        fat: result["food"]["nutrients"]["FAT"],
+        protein: result["food"]["nutrients"]["PROCNT"],
+        fibre: result["food"]["nutrients"]["FIBTG"]
+      })
+      @ingredients << ingredient if ingredient.valid?result
+    end
   end
 end
