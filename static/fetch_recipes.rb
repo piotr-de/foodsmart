8e2bd219dd9dc17b7cdb2825166999b7e7aa8ded
Piotr Damer <piotrd@czuby.net>
2020-03-19 14:56:00 +0000

    API fetch call script recreated in Ruby

---
+++ static/fetch_recipes.rb
@@ -0,0 +1,33 @@
+require 'rest-client'
+require 'json'
+
+path = "recipes"
+query = "burger"
+
+m = 0
+n = 99
+
+url = "https://www.edamam.com/search?q=#{query}&health=vegan&from=#{m}&to=#{n}"
+data = RestClient.get(url)
+count = JSON.parse(data)["count"]
+
+
+File.open(File.join(__dir__, path, "#{query}_#{m}_to_#{n}.json"), "w+") do |f|
+  f.write(data)
+end
+
+puts "No. of recipes: #{count}"
+
+if count > 99
+  m += 100
+  n += 100
+  while m < count
+    url = "https://www.edamam.com/search?q=#{query}&health=vegan&from=#{m}&to=#{n}"
+    File.open(File.join(__dir__, path, "#{query}_#{m}_to_#{n}.json"), "w+") do |f|
+      f.write(data)
+      puts "Saved file #{f.path}"
+    end
+    m += 100
+    n += 100
+  end
+end
